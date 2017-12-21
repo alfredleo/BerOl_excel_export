7ccc9b534f085399efd16e99cbe876f77c03794e
Alfred <alfredleo@gmail.com>
2017-12-21 21:55:17 +0500

    Add json to excel generator

---
+++ generateExcel.py
@@ -0,0 +1,32 @@
+import os
+import xlsxwriter
+
+
+directory = './output'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Create an new Excel file and add a worksheet.
+workbook = xlsxwriter.Workbook(directory + '/test.xlsx')
+worksheet = workbook.add_worksheet('Стройматериалы')
+
+# Widen the first column to make the text clearer.
+worksheet.set_column('A:A', 20)
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+
+# Write some simple text.
+worksheet.write('A1', 'Hello')
+
+# Text with formatting.
+worksheet.write('A2', 'World', bold)
+
+# Write some numbers, with row/column notation.
+worksheet.write(2, 0, 123)
+worksheet.write(3, 0, 123.456)
+
+# Insert an image.
+worksheet.insert_image('B5', 'logo.png')
+
+workbook.close()

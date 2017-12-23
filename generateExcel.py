fd01ad012ed7349411fd6f9d3ba96481b850b188
Alfred <alfredleo@gmail.com>
2017-12-23 21:04:23 +0500

    Add multiple json files and excel output.

--- generateExcel.py
+++ generateExcel.py
@@ -1,50 +1,75 @@
+# -*- coding: utf-8 -*-
 import os
-
 import xlsxwriter
 import json
+from xlsxwriter.utility import xl_rowcol_to_cell
 
-directory = './output'
-if not os.path.exists(directory):
-    os.makedirs(directory)
 
-inputJson = json.loads(open('input/краски.json', 'r', encoding='utf-8').read())
-for menu in inputJson:
-    for key, value in menu.items():
-        print()
-        print('[', key, ']')
-        # create folder output/{key}
-        for key1, value1 in value.items():
-            print('[', key1, ']')
-            # create excel file in output/{key} folder with {key1}.xlsx name
-            for key2, value2 in value1.items():
-                print('[', key2, ']')
-                # create new worksheet in above excel file with the name {key2}
-                for item in value2:
-                    print(item['name'], item['value'])
-                    # add fields in the current worksheet with the table caption {name} and field {value}
+def create_dir(directory_to_create):
+    """
+    Creates directory if it does not exist
+    :rtype: bool
+    """
+    if not os.path.exists(directory_to_create):
+        os.makedirs(directory_to_create)
+        return True
+    else:
+        return False
 
-# Create an new Excel file and add a worksheet.
-workbook = xlsxwriter.Workbook(directory + '/test.xlsx')
-worksheet = workbook.add_worksheet('Стройматериалы')
-worksheet2 = workbook.add_worksheet('Стройматериалы2')
 
-# Widen the first column to make the text clearer.
-worksheet.set_column('A:A', 20)
+out = './output/'
+input_json = './input/'
+create_dir(out)
+create_dir(input_json)
 
-# Add a bold format to use to highlight cells.
-bold = workbook.add_format({'bold': True})
+for file in os.listdir(input_json):
+    if file.endswith(".json"):
+        json_file = os.path.join(input_json, file)
+        inputJson = json.loads(open(json_file, 'r', encoding='utf-8').read())
+        workbooks = {}
+        for menu in inputJson:
+            for key, value in menu.items():
+                print()
+                print('[', key, ']')
+                # create folder output/{key}
+                create_dir(out + key)
+                for key1, value1 in value.items():
+                    print('[', key1, ']')
+                    # create excel file in output/{key} folder with {key1}.xlsx name
+                    path = out + key + '/' + key1 + '.xlsx'
+                    # check if workbook already created
+                    if path in workbooks.keys():
+                        w = workbooks[path]
+                    else:
+                        w = workbooks[path] = xlsxwriter.Workbook(path)
+                    # add formatting
+                    bold = w.add_format({'bold': True})
+                    center = w.add_format({'align': 'center'})
+                    dumpRows = 20
+                    for key2, value2 in value1.items():
+                        print('[', key2, ']')
+                        # create new worksheet in above excel file with the name {key2}
+                        worksheet = w.add_worksheet(key2[:31])
+                        for i in range(1, dumpRows + 1):
+                            worksheet.write(0, i, "[Товар %s]" % i, center)
+                    max1 = 0
+                    max2 = 0
+                    for idx, item in enumerate(value2):
+                        print(item['name'], item['value'])
+                        # get max field size
+                        if (len(item['name'])) > max1:
+                            max1 = len(item['name'])
+                        if (len(item['value'])) > max2:
+                            max2 = len(item['value'])
+                        # add fields in the current worksheet with the table caption {name} and field {value}
+                        worksheet.write(idx + 1, 0, item['name'], bold)
+                        worksheet.write(idx + 1, 1, item['value'])
+                    worksheet.set_column(1, 0, max1 + 4)
+                    worksheet.set_column(1, dumpRows, max2 + 3)
 
-# Write some simple text.
-worksheet.write('A1', 'Hello')
-
-# Text with formatting.
-worksheet.write('A2', 'World', bold)
-
-# Write some numbers, with row/column notation.
-worksheet.write(2, 0, 123)
-worksheet.write(3, 0, 123.456)
+        for key, workbook in workbooks.items():
+            print(key)
+            workbook.close()
 
 # Insert an image.
-worksheet.insert_image('B5', 'logo.png')
-
-workbook.close()
+# worksheet.insert_image('B5', 'logo.png')

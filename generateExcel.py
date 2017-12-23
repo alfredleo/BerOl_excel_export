acf7c1db6c1757df5828b14c79976d7a8cf7c493
Alfred <alfredleo@gmail.com>
2017-12-23 17:45:58 +0500

    add json format parsing

--- generateExcel.py
+++ generateExcel.py
@@ -1,17 +1,27 @@
 import os
+
 import xlsxwriter
 import json
-
-with open('menu.json') as json_data:
-    d = json.load(json_data)
-    print(d['Стройматериалы'])
-    # for item in d:
-    #     print(item)
-    print(json.dumps(d, sort_keys=True, indent=4))
 
 directory = './output'
 if not os.path.exists(directory):
     os.makedirs(directory)
+
+inputJson = json.loads(open('input/краски.json', 'r', encoding='utf-8').read())
+for menu in inputJson:
+    for key, value in menu.items():
+        print()
+        print('[', key, ']')
+        # create folder output/{key}
+        for key1, value1 in value.items():
+            print('[', key1, ']')
+            # create excel file in output/{key} folder with {key1}.xlsx name
+            for key2, value2 in value1.items():
+                print('[', key2, ']')
+                # create new worksheet in above excel file with the name {key2}
+                for item in value2:
+                    print(item['name'], item['value'])
+                    # add fields in the current worksheet with the table caption {name} and field {value}
 
 # Create an new Excel file and add a worksheet.
 workbook = xlsxwriter.Workbook(directory + '/test.xlsx')
